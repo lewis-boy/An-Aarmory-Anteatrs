9bfb146389c6da416989e5df2edc567f6c981891
Luis Escobar <luisaeniebla@gmail.com>
2023-05-30 22:00:10 -0700

    Solved Unpushable Button problem
    It needs to be a UI button in order for it to work. Just giving it the component button is not enough. Also made test script

---
+++ An Aarmory of Anteatrs/Assets/Scripts/Testing.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public void PushedButton(){
+        Debug.Log("This button was pushed!");
+    }
+}
